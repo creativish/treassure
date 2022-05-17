1d00b1a9cf4748ae0656d920d4e5d0b17fa2088c
Vishal Singh <>
2022-05-17 16:40:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to Treassure Island")
+print("Your mission to find the Treassure")
+level_1 = input('do you want go "left" or "right".?\n').lower()
+
+if level_1 == "left":
+  level_2 = input('you are come to lake do you want to'"swim" or "wait")
+  print("welcome to level 2")
+ #continue in the game
+else:
+ print("game over")
+ 
+
